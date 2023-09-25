bd148a8ba8456628650d39afad496b45d9d2533e
wlix13 <renat@mustafin.su>
2023-09-25 23:58:52 +0300

    DLinkedList

--- 2course/OS/hw_3/DLinkedList.c
+++ 2course/OS/hw_3/DLinkedList.c
@@ -17,117 +17,201 @@
 ввода), выводится в прямом и в обратном порядке, в цикле обрабатывается, вся
 динамически выделенная память освобождается.
 */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node {
   char *data;
-  struct Node *next;
   struct Node *prev;
+  struct Node *next;
 } Node;
 
-Node *createNode(char *data) {
-  Node *node = (Node *)malloc(sizeof(Node));
-  node->data = data;
-  node->next = NULL;
-  node->prev = NULL;
-  return node;
+typedef struct DoublyLinkedList {
+  Node *head;
+  Node *tail;
+} DoublyLinkedList;
+
+char *inputString() {
+  char *str;
+  int ch;
+  long size = 10;
+  long len = 0;
+  str = (char *)malloc(size);
+
+  while ((ch = getchar()) != EOF && ch != '\n') {
+    str[len++] = ch;
+    if (len == size) {
+      size *= 2;
+      str = (char *)realloc(str, size);
+    }
+  }
+  str[len++] = '\0';
+
+  return realloc(str, len);
 }
 
-Node *createList() {
-  Node *head = NULL;
-  Node *tail = NULL;
-  char *data = NULL;
-  while (1) {
-    printf("Enter data: ");
-    scanf("%ms", &data);
-    if (data[0] == '\0') {
-      break;
-    }
-    Node *node = createNode(data);
-    if (head == NULL) {
-      head = node;
-      tail = node;
+DoublyLinkedList *createList() {
+  DoublyLinkedList *list = (DoublyLinkedList *)malloc(sizeof(DoublyLinkedList));
+  list->head = NULL;
+  list->tail = NULL;
+  return list;
+}
+
+void freeList(DoublyLinkedList *list) {
+  Node *current = list->head;
+  while (current) {
+    Node *next = current->next;
+    free(current->data);
+    free(current);
+    current = next;
+  }
+  free(list);
+}
+
+void printList(DoublyLinkedList *list) {
+  Node *current = list->head;
+  while (current) {
+    printf("%s -> ", current->data);
+    current = current->next;
+  }
+  printf("NULL\n");
+}
+
+void printListReverse(DoublyLinkedList *list) {
+  Node *current = list->tail;
+  while (current) {
+    printf("%s -> ", current->data);
+    current = current->prev;
+  }
+  printf("NULL\n");
+}
+
+void deleteNodeByIndex(DoublyLinkedList *list) {
+  int index;
+  printf("Enter position to delete: ");
+  scanf("%d", &index);
+
+  if (!list->head)
+    return;
+
+  Node *current = list->head;
+  for (int i = 0; i < index && current; i++) {
+    current = current->next;
+  }
+
+  if (!current)
+    return;
+
+  if (current->prev) {
+    current->prev->next = current->next;
+  } else {
+    list->head = current->next;
+  }
+
+  if (current->next) {
+    current->next->prev = current->prev;
+  } else {
+    list->tail = current->prev;
+  }
+
+  free(current->data);
+  free(current);
+}
+
+void insertNodeAfterIndex(DoublyLinkedList *list) {
+  int index;
+  char data[128 + 1];
+  printf("Enter position to insert after: ");
+  scanf("%d", &index);
+  printf("Enter string to insert: ");
+  scanf("%s", data);
+
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = strdup(data);
+  newNode->prev = NULL;
+  newNode->next = NULL;
+
+  if (!list->head) {
+    list->head = newNode;
+    list->tail = newNode;
+    return;
+  }
+
+  Node *current = list->head;
+  for (int i = 0; i < index && current; i++) {
+    current = current->next;
+  }
+
+  if (!current) {
+    list->tail->next = newNode;
+    newNode->prev = list->tail;
+    list->tail = newNode;
+  } else {
+    newNode->next = current->next;
+    newNode->prev = current;
+    if (current->next) {
+      current->next->prev = newNode;
     } else {
-      tail->next = node;
-      node->prev = tail;
-      tail = node;
+      list->tail = newNode;
     }
-  }
-  return head;
-}
-
-void printList(Node *head) {
-  Node *node = head;
-  while (node != NULL) {
-    printf("%s ", node->data);
-    node = node->next;
-  }
-  printf("\n");
-}
-
-void printListReverse(Node *tail) {
-  Node *node = tail;
-  while (node != NULL) {
-    printf("%s ", node->data);
-    node = node->prev;
-  }
-  printf("\n");
-}
-
-void deleteList(Node *head) {
-  Node *node = head;
-  while (node != NULL) {
-    Node *next = node->next;
-    free(node->data);
-    free(node);
-    node = next;
+    current->next = newNode;
   }
 }
 
-void deleteNode(Node *node) {
-  if (node->prev != NULL) {
-    node->prev->next = node->next;
-  }
-  if (node->next != NULL) {
-    node->next->prev = node->prev;
-  }
-  free(node->data);
-  free(node);
-}
+void insertNodeAtBeginning(DoublyLinkedList *list, char *data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = strdup(data);
+  newNode->prev = NULL;
+  newNode->next = list->head;
 
-void deleteNodeByIndex(Node *head, int index) {
-  Node *node = head;
-  for (int i = 0; i < index; i++) {
-    node = node->next;
+  if (list->head) {
+    list->head->prev = newNode;
+  } else {
+    list->tail = newNode;
   }
-  deleteNode(node);
-}
 
-void insertNode(Node *node, char *data) {
-  Node *newNode = createNode(data);
-  newNode->next = node->next;
-  newNode->prev = node;
-  node->next = newNode;
-  if (newNode->next != NULL) {
-    newNode->next->prev = newNode;
-  }
-}
-
-void insertNodeByIndex(Node *head, int index, char *data) {
-  Node *node = head;
-  for (int i = 0; i < index; i++) {
-    node = node->next;
-  }
-  insertNode(node, data);
+  list->head = newNode;
 }
 
 int main() {
-  Node *head = createList();
-  printList(head);
-  printListReverse(head);
-  deleteList(head);
+  void (*functions[])(DoublyLinkedList *) = {
+      printList,
+      deleteNodeByIndex,
+      insertNodeAfterIndex,
+  };
+
+  DoublyLinkedList *head = createList();
+
+  printf("Enter words (one word per line, end with EOF):\n");
+  char *word;
+  while ((word = inputString()) && *word) {
+    insertNodeAtBeginning(head, word);
+    free(word);
+  }
+  // Clear last word
+  if (word != NULL) {
+    free(word);
+  }
+
+  // Summon flying menu
+  int choice;
+  do {
+    printf("\nMenu:\n");
+    printf("1. Print list\n");
+    printf("2. Delete node by index\n");
+    printf("3. Insert node after index\n");
+    printf("0. Exit\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= 3) {
+      functions[choice - 1](head);
+    }
+
+  } while (choice != 0);
+
+  freeList(head);
 
   return 0;
 }
